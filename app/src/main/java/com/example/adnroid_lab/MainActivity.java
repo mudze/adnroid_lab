c0762e1202cea3f1caaf5ebfdec61224ea0387c1
mudze <burzynski.piotr1@gmail.com>
2019-03-25 19:55:09 +0100

    Dodanie ListView

--- app/src/main/java/com/example/adnroid_lab/MainActivity.java
+++ app/src/main/java/com/example/adnroid_lab/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.adnroid_lab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> target;
+    private ArrayAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] values = new String[] {
+                "Jabłko", "Pomarańcza", "Gruszka", "Pietruszka",
+                "Marchew", "Rzodkiew", "Cebula", "Kapusta",
+                "Seler", "Mandarynka", "Śliwka", "Granat",
+                "Awokado", "Brzoskwinia", "Nektarynka", "Banan"
+        };
+
+        this.target = new ArrayList<>();
+        this.target.addAll(Arrays.asList(values));
+
+        this.adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, this.target);
+
+        ListView listview = findViewById( R.id.listView);
+        listview.setAdapter(this.adapter);
     }
 }
